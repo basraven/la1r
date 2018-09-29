99e434ab5ed4270763edbf6cb7601b64a124a998
Sebastiaan Raven <basraven@gmail.com>
2018-09-29 15:03:14 +0200

    works with raspi1

--- ansible-client/run.sh
+++ ansible-client/run.sh
@@ -1,4 +1,4 @@
 docker network create --driver bridge ansible-testnet
 docker rm -f ansible-control
 docker build . -t ansible-control
-docker run -it --name ansible-control -v $pwd/../playbooks/:/playbooks --net ansible-testnet ansible-control /bin/bash
+docker run -it --name ansible-control -v $pwd/../playbooks/:/playbooks -v $pwd/../hosts:/etc/ansible/hosts --net ansible-testnet ansible-control /bin/bash
--- raspberry-pi-test-env/run.sh
+++ raspberry-pi-test-env/run.sh
@@ -1,9 +1,6 @@
 docker network create --driver bridge ansible-testnet
-docker build . -t raspi-test
+docker build . -t raspi
 docker rm raspi1 raspi2 raspi3 --force
-docker run --name raspi1 --net ansible-testnet -td raspi-test /bin/bash
-docker run --name raspi2 --net ansible-testnet -td raspi-test /bin/bash
-docker run --name raspi3 --net ansible-testnet -td raspi-test /bin/bash
-docker exec raspi1 service ssh start
-docker exec raspi2 service ssh start
-docker exec raspi3 service ssh start
+docker run --name raspi1 --hostname raspi1 --net ansible-testnet -d raspi
+docker run --name raspi2 --hostname raspi2 --net ansible-testnet -d raspi
+docker run --name raspi3 --hostname raspi3 --net ansible-testnet -d raspi
