4cc98f5548ad4eb31a0a575fabd901843b800e50
Sebastiaan Raven <basraven@gmail.com>
2018-10-21 00:06:36 +0200

    - simplified run.ps1 - improved docker install (working now)

--- run.ps1
+++ run.ps1
@@ -1,4 +1,3 @@
-docker network create --driver bridge ansible-testnet
 docker rm -f ansible-control
 docker build . -t ansible-control
-docker run -it --name ansible-control --hostname control  -v $pwd/playbooks/:/playbooks -v $pwd/credentials:/credentials --net ansible-testnet ansible-control /bin/bash
+docker run -it --name ansible-control --hostname control  -v $pwd/playbooks/:/playbooks -v $pwd/credentials:/credentials ansible-control /bin/bash
