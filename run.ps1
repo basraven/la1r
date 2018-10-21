575863f24047495aa5a0cef6f6419f5b562dbbd6
Sebastiaan Raven <basraven@gmail.com>
2018-10-21 23:36:47 +0200

    Added kubernetes deployments

--- run.ps1
+++ run.ps1
@@ -1,3 +1,7 @@
 docker rm -f ansible-client
 docker build ./ansible-client -t ansible-client
-docker run -it --rm --name ansible-client --hostname ansible-client  -v $pwd/playbooks/:/playbooks -v $pwd/credentials:/credentials ansible-client /bin/bash
+docker run -it --rm --name ansible-client --hostname ansible-client \
+    -v $pwd/playbooks/:/playbooks \
+    -v $pwd/kubernetes/:/kubernetes \
+    -v $pwd/credentials:/credentials \
+    ansible-client /bin/bash
