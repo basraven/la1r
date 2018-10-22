1540200e44563da081f9ae4016e4bae0c2f8483c
Sebastiaan Raven <basraven@gmail.com>
2018-10-23 00:50:01 +0200

    - Should work with actual pi now instead of simulzation - Added become: True by default

--- run.ps1
+++ run.ps1
@@ -1,7 +1,3 @@
 docker rm -f ansible-client
 docker build ./ansible-client -t ansible-client
-docker run -it --rm --name ansible-client --hostname ansible-client \
-    -v $pwd/playbooks/:/playbooks \
-    -v $pwd/kubernetes/:/kubernetes \
-    -v $pwd/credentials:/credentials \
-    ansible-client /bin/bash
+docker run -it --rm --name ansible-client --hostname ansible-client -v $pwd/playbooks/:/playbooks -v $pwd/kubernetes/:/kubernetes -v $pwd/credentials:/credentials ansible-client /bin/bash
