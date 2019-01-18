bfff0d79d5527352c2162b4f80c47272bd9f9f4d
Sebastiaan Raven <basraven@gmail.com>
2019-01-18 02:21:22 +0100

    Nextcloud is working now, time to sleep

--- kubernetes/nextcloud/config/init.sql
+++
@@ -1,4 +0,0 @@
--- CREATE USER 'username'@'localhost' IDENTIFIED BY 'password';
-CREATE DATABASE IF NOT EXISTS nextcloud;
--- GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON nextcloud.* TO 'username'@'localhost' IDENTIFIED BY 'password';
-FLUSH privileges;
