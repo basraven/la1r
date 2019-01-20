4530867a33f48a67e967da644f77cd0cb4afac22
Sebastiaan Raven <basraven@gmail.com>
2019-01-20 11:58:05 +0100

    dnsmasq works!!!!!!

--- playbooks/roles/dnsmasq/README.md
+++
@@ -1,3 +0,0 @@
-# DNSMASQ
-Needs to be placed on a dedicated server, Raspi3 with K8s is too slow it seems.
-The Dockerfile is not needed
--- playbooks/roles/dnsmasq/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build . -t dnsmasq
-docker rm -f dnsmasq
-docker run -d --name dnsmasq --restart always --net=host -v /home/basraven/dnsmasq/etc/:/etc/dnsmasq.d --cap-add=NET_ADMIN dnsmasq
