a5b756e12e0696b77db6b4decf3fcea842d24626
Sebastiaan Raven <basraven@gmail.com>
2019-01-22 01:13:10 +0100

    - added redis support, working! - improved nextcloud and mariadb a little bit

---
+++ kubernetes/nextcloud/config/apcu.config.php
@@ -0,0 +1,9 @@
+<?php
+$CONFIG = array (
+  'memcache.local' => '\OC\Memcache\Redis',
+  'redis' => array(
+    'host' => 'redis',
+    'port' => 6379,
+    ),
+  'memcache.locking' => '\OC\Memcache\Redis',
+);
--- kubernetes/nextcloud/config/set-nextcloud.sh
+++ kubernetes/nextcloud/config/set-nextcloud.sh
@@ -2,5 +2,6 @@
 set -eu
 echo "Tuning the container..."
 a2enmod http2
+a2enmod ssl
 echo "Protocols h2 h2c http/1.1" >> /etc/apache2/apache2.conf
 /entrypoint.sh apache2-foreground
