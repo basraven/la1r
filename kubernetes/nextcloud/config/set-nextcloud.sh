33641abb1b853fdc1209040a9862d8b500ad137e
Sebastiaan Raven <basraven@gmail.com>
2019-01-30 21:36:15 +0100

    - added Jackett - changed the prometheus settings - fixing occ

--- kubernetes/nextcloud/config/set-nextcloud.sh
+++ kubernetes/nextcloud/config/set-nextcloud.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -eu
 echo "Tuning the container..."
+echo "alias occ='su -l www-data -s /var/www/html/occ'" >> ~/.bashrc
 a2enmod http2
 a2enmod ssl
 echo "Protocols h2 h2c http/1.1" >> /etc/apache2/apache2.conf
