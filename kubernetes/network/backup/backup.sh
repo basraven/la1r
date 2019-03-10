1228918c368e85c0c4cb916b800886d47effbb16
Sebastiaan Raven <basraven@gmail.com>
2019-03-10 12:16:08 +0100

    Backup is working!

--- kubernetes/network/backup/backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "testen..."
-# rsync -r --no-perms --no-owner --no-group --delete --force --update --progress -e "ssh -i /backup-ssh-key/id_rsa backup@10.8.0.18" /backup-ssh-key /tmp
-mkdir -p ~/.ssh
-cp /backup-ssh-key/id_rsa ~/.ssh/id_rsa
-ssh backup@raspi2 'ls /'
