f1dba06256dfa29ad1af690d9c0f65c92c5286d4
Sebastiaan Raven <basraven@gmail.com>
2019-05-28 23:05:46 +0200

    - small backup script mods

--- kubernetes/backup/nextcloud/backup-monthly.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Starting rsync file sync..."
-rsync -a --no-perms --no-owner --no-group --delete --force --update --progress /nextcloud-web /backup/nextcloud/monthly
-rsync -a --no-perms --no-owner --no-group --delete --force --update --progress /nextcloud-data /backup/nextcloud/monthly
