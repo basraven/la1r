b52c008c707c6b890c224f07985143b2a730ddbf
Sebastiaan Raven <basraven@gmail.com>
2019-10-06 02:47:07 +0200

    - cleaned up nextcloud yaml - fixed ssl and online traefik, it's working now - added grafana dashboard

--- kubernetes/nextcloud/config/apcu.config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$CONFIG = array (
-  'memcache.local' => '\OC\Memcache\Redis',
-  'redis' => array(
-    'host' => 'redis',
-    'port' => 6379,
-    ),
-  'memcache.locking' => '\OC\Memcache\Redis',
-);
