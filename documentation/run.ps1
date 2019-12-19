216a44932a1efdda35970c0dba9858bf20ca96c9
Sebastiaan Raven <basraven@gmail.com>
2019-12-19 11:25:22 +0100

    Added introduction menu item

--- documentation/run.ps1
+++ documentation/run.ps1
@@ -1,2 +1,3 @@
 cd ..
-docker run --rm -it --name la1r -p 8313:8313 --workdir /go/src/docs/src -v $pwd/:/go/src -it golang:alpine /bin/sh -c "/go/src/docs/install-hugo.sh ; hugo server --bind '0.0.0.0' --port 8313 --navigateToChanged --disableFastRender --forceSyncStatic"
+docker run --rm -it --name la1r -p 8313:8313 --workdir /go/src/documentation/src -v $pwd/:/go/src -it golang:alpine /bin/sh
+/go/src/documentation/install-hugo.sh ; hugo server --bind '0.0.0.0' --port 8313 --navigateToChanged --disableFastRender --forceSyncStatic
--- documentation/src/content/_index.md
+++ documentation/src/content/_index.md
@@ -1,5 +1,5 @@
 ---
-title: Overview
+title: Introduction
 type: docs
 bookToc: false
 ---
