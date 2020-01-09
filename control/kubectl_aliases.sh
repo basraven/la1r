b08412ffbb1ff69f27dd4382660de0f6dd8af985
Sebastiaan Raven <basraven@gmail.com>
2020-01-09 22:35:44 +0100

    more kafka

--- control/kubectl_aliases.sh
+++ control/kubectl_aliases.sh
@@ -1,18 +1,10 @@
 alias k='kubectl'
-alias kg='kubectl get'
+alias kns='kubectl config set-context --current --namespace'
+alias kgp='kubectl get pods'
+alias wkgp='watch kubectl get pods'
 alias ka='kubectl apply -f'
-alias kgpo='kubectl get pod'
-
-alias ksysgpo='kubectl --namespace=kube-system get pod'
 
 alias krm='kubectl delete'
 alias krmf='kubectl delete -f'
-alias krming='kubectl delete ingress'
-alias krmingl='kubectl delete ingress -l'
-alias krmingall='kubectl delete ingress --all-namespaces'
 
-alias kgsvcoyaml='kubectl get service -o=yaml'
-alias kgsvcwn='watch kubectl get service --namespace'
-alias kgsvcslwn='watch kubectl get service --show-labels --namespace'
-
-alias kgwf='watch kubectl get -f'
+alias kw='watch kubectl get -f'
