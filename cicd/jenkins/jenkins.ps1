03dd5ceeeccf784623c63445aa2fa2d7a6e620c6
Sebastiaan Raven <basraven@gmail.com>
2020-07-02 01:47:20 +0200

    more jenkins config and plugins

--- cicd/jenkins/jenkins.ps1
+++ cicd/jenkins/jenkins.ps1
@@ -1,3 +1,3 @@
 docker rmi la1r/jenkins
 docker build . -t la1r/jenkins
-docker run --rm -it -v $pwd/:/seb -v $pwd/jenkins.yaml:/var/jenkins_home/casc_configs/jenkins.yaml -p 8080:8080 -p 50000:50000 la1r/jenkins
+docker run --rm --privileged -it -v $pwd/:/seb -v $pwd/jenkins.yaml:/var/jenkins_home/casc_configs/jenkins.yaml -p 8080:8080 -p 50000:50000 la1r/jenkins
