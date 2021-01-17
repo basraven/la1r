a714006c81360fa44ea13ca0de1478f8e471de30
Sebastiaan Raven <basraven@gmail.com>
2021-01-17 19:49:46 +0100

    working!!

--- brian/spark/zeppelinrun.sh
+++ brian/spark/zeppelinrun.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker build . -t zep
-docker run -it -p 8088:80  -v /home/basraven/.kube/config:/root/.kube/config --rm zep bash 
+SPARK_VERSION=3.0.1
+docker build . -t docker.io/basraven/zeppelin:$SPARK_VERSION
+docker run -it -p 8088:80  -v /home/basraven/.kube/config:/root/.kube/config --rm docker.io/basraven/zeppelin:$SPARK_VERSION bash 
