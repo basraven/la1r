79e75243d2afa21f1fa6a39e5a42f9333763c576
Sebastiaan Raven <basraven@gmail.com>
2025-08-12 23:44:51 +0200

    Enhance Kubernetes configurations with various updates
    - Added a nonessential label to node jay-c in local-path-provisioner.
    - Introduced environment variables for Jackett to disable IPv6 and set CLI arguments for public listening.
    - Uncommented and restructured the Jellyfin LoadBalancer service definition for clarity.
    - Commented out the log file path in Traefik configuration to prevent unnecessary logging.
    - Added WG_ALLOWED_IPS environment variable to wg-easy to allow all IPs.

--- kubernetes/storage/local-path-provisioner/node-labels/node-labels.sh
+++ kubernetes/storage/local-path-provisioner/node-labels/node-labels.sh
@@ -7,6 +7,7 @@ kubectl taint node stephanie la1r.workload/specificonly=true:NoSchedule
 
 # Labels
 kubectl label nodes jay-c la1r.workload/essential=true
+kubectl label nodes jay-c la1r.workload/nonessential=true
 kubectl label nodes linux-wayne la1r.workload/nonessential=true
 kubectl label nodes stephanie la1r.workload/specificonly=true
 
