a0716b1961dfe4c0dd2b94b3b1c3ed640f9a332c
Sebastiaan <basraven@gmail.com>
2025-08-19 21:25:48 +0200

    Moved folders around to not have  kubernetes-todeploy anymore
    Add Traefik Ingress Controller and related resources for online namespace
    
    - Create IngressClass, ClusterRole, ServiceAccount, ClusterRoleBinding, Deployment, and Service for Traefik ingress controller.
    - Implement rate limiting and HTTP to HTTPS redirection middlewares.
    - Define LoadBalancer service for online traffic.
    - Add Certificate resource for TLS configuration.
    - Set up IngressRoutes for both non-TLS and TLS traffic for online services.
    
    Create PersistentVolumeClaim for Traefik configuration
    
    - Define PVC for storing Traefik configuration data.
    
    Deploy Shields service with IngressRoutes
    
    - Add Deployment and Service for Shields application.
    - Configure IngressRoutes for both non-TLS and TLS traffic.
    
    Implement Shieldsk8s service with necessary RBAC
    
    - Create ServiceAccount, ClusterRole, and ClusterRoleBindings for Shieldsk8s.
    - Deploy Shieldsk8s application with appropriate permissions.
    
    Remove deprecated backup and namespace configurations
    
    - Delete unused backup namespace and related kustomization files.
    - Remove multiple unused namespaces from deployment configurations.
    
    Clean up unused Kubernetes resources
    
    - Remove old deployments, services, and PVCs for Bazarr and Couchpotato applications.
    - Delete associated kustomization and namespace files for torrent namespace.
    
    Remove OpenVPN authentication test scripts
    
    - Delete init scripts and environment variables related to OpenVPN authentication testing.
    
    Remove utility scripts for deleting evicted and unknown pods
    
    - Clean up scripts that were used for managing pod states in the current namespace.

--- todeploy-kubernetes/scripts/deleteEvictedInCurrentNS.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for each in $(kubectl get pods |grep Evicted|awk '{print $1}');
-do
-  #echo $each
-  kubectl delete pods $each
-done
--- todeploy-kubernetes/scripts/deleteUnknownInCurrentNS.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for each in $(kubectl get pods |grep Unknown|awk '{print $1}');
-do
-  #echo $each
-  kubectl delete pods $each
-done
