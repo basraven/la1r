785d97684640661ce0b8b986dbc0470efbd04f16
Sebastiaan Raven <basraven@gmail.com>
2025-08-31 00:49:52 +0200

    feat(kubernetes-dashboard): enhance dashboard setup with token creation, Helm installation, and service configurations; remove oauth2-proxy resources

--- kubernetes/kubernetes-dashboard/get-kubernetes-dashboard-token.sh
+++ kubernetes/kubernetes-dashboard/get-kubernetes-dashboard-token.sh
@@ -1,1 +1,2 @@
+kubectl -n kubernetes-dashboard create token kubernetes-dashboard
 kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
---
+++ kubernetes/kubernetes-dashboard/install-dashboard-helm.sh
@@ -0,0 +1,7 @@
+# Add kubernetes-dashboard repository
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+# Deploy a Helm Release named "kubernetes-dashboard" using the kubernetes-dashboard chart
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard \
+  --create-namespace --namespace kubernetes-dashboard \
+  --set app.ingress.enabled=false \
+  --set kong.proxy.http.enabled=true
