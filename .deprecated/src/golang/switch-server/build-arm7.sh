23ba6b07a818cce28220b53f0eb28abab5281e3b
Sebastiaan Raven <basraven@gmail.com>
2026-09-06 15:41:57 +0000

    chore(deprecated): remove golang switch-server source

--- .deprecated/src/golang/switch-server/build-arm7.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-GoVersion=1.23
-docker run --rm -v $PWD:/app -w /app/cmd/switch-server -e GOOS=linux -e GOARCH=arm -e GOARM=7 -it golang:$GoVersion-alpine go build -v -o ../../bin/switch-server-arm7
--- .deprecated/src/golang/switch-server/dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-GoVersion=1.23
-docker run -v $PWD:/app -w /app -it golang:$GoVersion-alpine sh
